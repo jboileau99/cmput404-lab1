d50976a9c00ac2cd78d76c31d5f86f62a6851b89
Justin Boileau <justin@blindnet.io>
2023-01-11 17:50:56 -0700

    make script get and print itself

--- lab1.py
+++ lab1.py
@@ -7,6 +7,6 @@ print(requests.__version__)
 response = requests.get("https://google.com")
 print(response.text)
 
-# Make GET request to our own script
-myScript = requests.get("https://github.com/jboileau99/cmput404-lab1/blob/main/lab1.py")
+# Make GET request to our own script and print its source code
+myScript = requests.get("https://raw.githubusercontent.com/jboileau99/cmput404-lab1/c94ac82be1d79afb0c2a153b50750558f837067e/lab1.py?token=GHSAT0AAAAAAB5FBQVQCUP4Y7YVKJGDHW6GY57M4TQ")
 print(myScript.text)
