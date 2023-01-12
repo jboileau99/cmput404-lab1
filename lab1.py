c94ac82be1d79afb0c2a153b50750558f837067e
Justin Boileau <justin@blindnet.io>
2023-01-11 17:42:32 -0700

    updated script to get and print itself

--- lab1.py
+++ lab1.py
@@ -8,4 +8,5 @@ response = requests.get("https://google.com")
 print(response.text)
 
 # Make GET request to our own script
-myScript = requests.get()
+myScript = requests.get("https://github.com/jboileau99/cmput404-lab1/blob/main/lab1.py")
+print(myScript.text)
